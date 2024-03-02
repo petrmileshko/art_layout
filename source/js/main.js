2d20df28ec3bbb3da84f2641380f9edebdd4c3f4
Peter <mileshko.petr@yandex.ru>
2024-03-02 20:00:21 +0300

    updated

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,19 @@
 import onLoadJs from './utils/utils.js';
+import menuMobile from './menu/menu.js';
 
+const initHeader = onLoadJs('.page__header', '--nojs');
 
+initHeader();
+
+const initMobileMenu = menuMobile({
+  element: '.page__header',
+  activation: 'page__header--opened'
+}, {
+  element: '.navigation__menu',
+  activation: 'navigation__menu--opened'
+}, {
+  element: '.burger__icon',
+  activation: 'burger__icon--opened'
+});
+
+initMobileMenu();
